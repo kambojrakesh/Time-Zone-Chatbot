1eeb1481c121a168e2410f20050365fc4189c1f8
KambojRakesh <rakeshkamboj91@gmail.com>
2021-07-11 22:54:15 +0530

    added time zone based chatbot

--- actions/actions.py
+++ actions/actions.py
@@ -7,21 +7,34 @@
 
 # This is a simple example for a custom action which utters "Hello World!"
 
-# from typing import Any, Text, Dict, List
-#
-# from rasa_sdk import Action, Tracker
-# from rasa_sdk.executor import CollectingDispatcher
-#
-#
-# class ActionHelloWorld(Action):
-#
-#     def name(self) -> Text:
-#         return "action_hello_world"
-#
-#     def run(self, dispatcher: CollectingDispatcher,
-#             tracker: Tracker,
-#             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-#
-#         dispatcher.utter_message(text="Hello World!")
-#
-#         return []
+from typing import Any, Text, Dict, List
+
+from rasa_sdk import Action, Tracker
+from rasa_sdk.executor import CollectingDispatcher
+
+timezones = {
+    "Hisar":"UTC+1",
+    "Sirsa": "UTC+2",
+    "Jind":"UTC+3",
+    "Delhi":"UTC+4"
+}
+
+class ActionShowTimeZone(Action):
+
+    def name(self) -> Text:
+        return "action_show_time_zone"
+
+    def run(self, dispatcher: CollectingDispatcher,
+            tracker: Tracker,
+            domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
+
+        city = tracker.get_slot("city")
+        timezone = timezones.get(city)
+
+        if timezone is None:
+            output = "could not find the timezone of {}".format(city)
+        else: 
+            output = "Timezone of {} is {}".format(city, timezone)
+        
+        dispatcher.utter_message(text=output)
+        return []
